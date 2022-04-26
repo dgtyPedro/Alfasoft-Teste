5e1c2269cf45199ef2f12b8bec3506bc452288f5
dgtyPedro <pedrogcs0104@gmail.com>
2022-04-26 09:52:17 -0300

    create contact model and createcontact controller

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -4,7 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Contact;
+
 class Main extends Controller
 {
-    //
+    public function Home()
+    {
+        $data['contacts'] = Contact::all();
+        return view('home', $data);
+    }
+
+    public function CreateContact(Request $request)
+    {
+        if($request->post('_token'))
+        {
+            $isvalidated = $request->validate([
+                'name' => 'required|min:5',
+                'contact' => 'required|digits:9',
+                'email_address' => 'required|email:rfc,dns'
+            ]);
+
+            if($isvalidated)
+            {
+                $concact = Contact::create($request->except('_token'));
+            }else{
+
+            }
+            
+        }
+
+        return view('createcontact');
+    }
 }
