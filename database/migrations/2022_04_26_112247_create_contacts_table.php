fdfcc06216e06f2d4a6744f623322793502d138d
dgtyPedro <pedrogcs0104@gmail.com>
2022-04-26 11:17:18 -0300

    change timestamps

--- database/migrations/2022_04_26_112247_create_contacts_table.php
+++ database/migrations/2022_04_26_112247_create_contacts_table.php
@@ -18,7 +18,7 @@ class CreateContactsTable extends Migration
             $table->string('name');
             $table->string('contact', 9);
             $table->string('email_address');
-            $table->timestamps();
+            $table->timestampsTz();
         });
     }
 
